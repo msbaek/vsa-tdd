48e043a9963daea003da5ebf3dcbfc2dcfeb825d
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-12 15:37:16 +0900

    Add runtime graphql wiring configurer

--- src/main/java/com/example/vsatdd/VsaTddApplication.java
+++ src/main/java/com/example/vsatdd/VsaTddApplication.java
@@ -1,7 +1,10 @@
 package com.example.vsatdd;
 
+import graphql.scalars.ExtendedScalars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @SpringBootApplication
 public class VsaTddApplication {
@@ -10,4 +13,11 @@ public class VsaTddApplication {
 		SpringApplication.run(VsaTddApplication.class, args);
 	}
 
+	@Bean
+	public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+		return wiringBuilder -> wiringBuilder
+				.scalar(ExtendedScalars.GraphQLBigDecimal)
+				.scalar(ExtendedScalars.DateTime)
+				.scalar(ExtendedScalars.Date);
+	}
 }
