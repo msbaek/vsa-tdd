8fd7ee9a740bc240f11e212173ffc053136873e5
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-12 15:31:18 +0900

    project created

---
+++ src/main/java/com/example/vsatdd/VsaTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.vsatdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsaTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vsatdd/VsaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vsatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
